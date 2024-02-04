5af73bfef58b30da2a623b8cad9b852565d58230
“Viktoriia <victoriia.boiechko@gmail.com>
2024-02-04 15:25:06 +0000

    Chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
+    path('about/', views.about, name = 'about'),
     path('', views.index, name = 'index'),
+    path('greetings/', views.greet, name = 'greet')
 ]
